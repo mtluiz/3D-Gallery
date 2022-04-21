b263950e465af0f8cce225fb185541cd43847f7b
Matheus Madureira <matheusluizn@gmail.com>
2022-04-21 12:32:03 -0300

    feat: trying some "shady" stuff

--- src/screen.js
+++ src/screen.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import fragment from './shaders/fragment.fs.glsl';
-import fragment from './shaders/vertex.vs.glsl';
+import vertex from './shaders/vertex.vs.glsl';
 
 export default class Screen {
     constructor(options) {
@@ -10,6 +10,7 @@ export default class Screen {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.setPixelRatio(window.devicePixelRatio)
         this.scene = new THREE.Scene();
+        this.time = 0;
 
         this.camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1)
         this.camera.position.z = 5
@@ -29,16 +30,28 @@ export default class Screen {
     }
 
     createGeometries() {
-        const geometry = new THREE.BoxGeometry(0.4, 0.4, 0.4);
-        const material = new THREE.MeshBasicMaterial({ color: 0xff00ff })
-        this.box = new THREE.Mesh(geometry, material);
+        const geometry = new THREE.PlaneBufferGeometry(0.5, 0.5, 100, 100);
+        this.material = new THREE.ShaderMaterial({
+            wireframe: true, 
+                uniforms:{
+                    time: {value: 1.0},
+                    resolution: {value: new THREE.Vector2()}
+                },
+                vertexShader: vertex,
+                fragmentShader: fragment
+
+        })
+        this.box = new THREE.Mesh(geometry, this.material);
 
         this.scene.add(this.box)
     }
 
     animate() {
+        this.time += 0.1
+        console.log(this.time)
+        this.material.uniforms.time.value = this.time
+
         this.renderer.render(this.scene, this.camera)
-        this.box.rotation.x += 0.01
         requestAnimationFrame(this.animate.bind(this))
     }
 

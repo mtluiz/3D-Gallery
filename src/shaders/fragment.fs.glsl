1acfb7c5ba45c77c71dc3bb3dcfbddfa49523191
Matheus Madureira <matheusluizn@gmail.com>
2022-05-08 16:51:34 -0300

    feat: creating webgl shaders

--- src/shaders/fragment.fs.glsl
+++ src/shaders/fragment.fs.glsl
@@ -1,5 +1,6 @@
-varying float pulse;
+uniform sampler2D uTexture;
+varying vec2 vUv;
 
 void main(){
-    gl_FragColor = vec4(1.0, pulse, 0.0, 1.0);
+    gl_FragColor = texture2D(uTexture, vUv);
 }
--- src/shaders/vertex.vs.glsl
+++ src/shaders/vertex.vs.glsl
@@ -1,9 +1,30 @@
 uniform float time;
-varying float pulse;
+uniform float uProgress;
+uniform vec2 uResolution;
+uniform vec2 uQuadSize;
+uniform vec4 uCorners;
+varying vec2 vSize;
 
+varying vec2 vUv;
 void main() {
-    vec3 newPosition = position;
-    newPosition.z = 0.1*sin(length(position)*30. + time);
-    pulse = 10. * newPosition.z;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+    float PI = 3.1415926;
+    vUv = uv;
+    float sine = sin(PI*uProgress);
+    float waves = sine*0.1*sin(5.*length(uv) + 15.*uProgress);
+    vec4 defaultState = modelMatrix*vec4( position, 1.0 );
+    vec4 fullScreenState = vec4( position, 1.0 );
+    fullScreenState.x *=uResolution.x/uQuadSize.x;
+    fullScreenState.y *=uResolution.y/uQuadSize.y;
+    float cornersProgress = mix(
+        mix(uCorners.z,uCorners.w,uv.x),
+        mix(uCorners.x,uCorners.y,uv.x),
+        uv.y
+    );
+
+
+    vec4 finalState = mix(defaultState,fullScreenState,uProgress + waves);
+
+    vSize = mix(uQuadSize,uResolution,uProgress);
+
+    gl_Position = projectionMatrix * viewMatrix * finalState;
 }

c1de36377447c66afc0aefa6967ed58944e523d3
Matheus Madureira <matheusluizn@gmail.com>
2022-05-08 16:51:19 -0300

    feat: starting asscroll invocation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import Screen from "./screen";
+import './scss/style.scss'
+import ASScroll from "@ashthornton/asscroll";
 
-new Screen({domElement: document.getElementById('screen'), debug: true})
+new Screen({domElement: document.querySelector('#container'), debug: true})
+
+
+const asscroll = new ASScroll();
+
+window.addEventListener('load', ()=> {
+    asscroll.enable({
+        horizontalScroll: true
+    })
+}, {passive: true})
